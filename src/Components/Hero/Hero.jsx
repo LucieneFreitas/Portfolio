11ff99e76afc39c9ccc49a1d8587e8a0356a41b9
LucieneFreitas <luciene_freitas@yahoo.com.br>
2024-09-25 08:35:58 -0300

    criado hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,8 +5,12 @@ export function Hero() {
   return (
     <div className='hero'>
       <img src={profile_img} alt="" />
-      <h1>Olá, Sou Luciene, Desenvolvedora Frontend e Web Designer</h1>
-      <p></p>
+      <h1>Olá, Sou <span>Luciene Freitas</span></h1>
+      <h2>Desenvolvedora Frontend e Web Designer</h2>
+      <div className="hero-action">
+        <div className="hero-connect">Contate-me</div>
+        <div className="hero-resume">Meu Currículo</div>
+      </div>
     </div>
   )
 }
